1a1d1dabce048a4256fb0a5f409308051d521990
DimitarVezalov <69586456+DimitarVezalov@users.noreply.github.com>
2021-01-22 18:52:25 +0200

    Delete MsSql/BuiltInFunctions/Exercise directory

--- MsSql/BuiltInFunctions/Exercise/P13.MixOfPeakAndRiverNames.sql
+++
@@ -1,6 +0,0 @@
-SELECT PeakName, 
-	   RiverName,
-	   LOWER(CONCAT(SUBSTRING(p.PeakName,1, LEN(p.PeakName) - 1), r.RiverName)) AS [Mix]
-	FROM Peaks AS p
-	JOIN Rivers AS r ON RIGHT(p.PeakName,1) = LEFT(r.RiverName, 1)
-	ORDER BY Mix
